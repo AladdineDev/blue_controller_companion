e1109f1a6740f2fbac044f642efdf8995b02d1ce
AladdineDev <aaldn.dev@gmail.com>
2024-07-10 22:27:23 +0200

    feat: create login screen and controller list screen

---
+++ lib/features/controller/presentation/controller_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ControllerListScreen extends StatelessWidget {
+  const ControllerListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
