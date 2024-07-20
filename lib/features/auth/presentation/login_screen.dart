f19e2a4fa162d9f09cc7e2527f6d9fb63bdaa0f5
AladdineDev <aaldn.dev@gmail.com>
2024-07-20 03:37:14 +0200

    feat: implement login screen

---
+++ lib/features/auth/presentation/login_screen.dart
@@ -0,0 +1,92 @@
+import 'package:blue_controller_companion/extensions/build_context_extension.dart';
+import 'package:blue_controller_companion/features/auth/data/repository/auth_repository.dart';
+import 'package:blue_controller_companion/features/auth/domain/login_input_model.dart';
+import 'package:blue_controller_companion/features/auth/presentation/widgets/login_form_card.dart';
+import 'package:blue_controller_companion/router/router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class LoginScreen extends HookConsumerWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, boxConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: boxConstraints.maxHeight,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 80,
+                    horizontal: 16,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        "Blue controller companion",
+                        style: context.textTheme.displayMedium,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 100),
+                      SizedBox(
+                        width: 400,
+                        child: MyLoginFormCard(
+                          emailController: emailController,
+                          passwordController: passwordController,
+                        ),
+                      ),
+                      const SizedBox(height: 48),
+                      FilledButton(
+                        onPressed: () => _onSignInButtonPressed(
+                          context,
+                          ref,
+                          emailController: emailController,
+                          passwordController: passwordController,
+                        ),
+                        child: const Text("Sign in"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  _onSignInButtonPressed(
+    BuildContext context,
+    WidgetRef ref, {
+    required TextEditingController emailController,
+    required TextEditingController passwordController,
+  }) async {
+    final loginInput = LoginInputModel(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+    try {
+      await ref.read(authRepositoryProvider).signIn(loginInput: loginInput);
+      if (!context.mounted) return;
+      const ControllerListScreenRoute().go(context);
+    } catch (e) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+    }
+  }
+}
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,5 @@
-import 'package:blue_controller_companion/exceptions/app_exception.dart';
 import 'package:blue_controller_companion/features/controller/presentation/controller_list_screen.dart';
-import 'package:blue_controller_companion/features/login/data/repository/auth_repository.dart';
-import 'package:blue_controller_companion/features/login/presentation/login_screen.dart';
+import 'package:blue_controller_companion/features/auth/presentation/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -13,18 +11,19 @@ GoRouter router(RouterRef ref) {
   return GoRouter(
     routes: $appRoutes,
     initialLocation: '/login',
-    redirect: (context, state) async {
-      final authRepository = ref.watch(authRepositoryProvider);
-      if (state.fullPath == "/login") {
-        try {
-          await authRepository.reSignIn();
-          return "/controllers";
-        } on LoginSessionExpired catch (_) {
-          return "/login";
-        }
-      }
-      return state.path;
-    },
+    // redirect: (context, state) async {
+    // TODO: Implement redirect
+    //   final authRepository = ref.watch(authRepositoryProvider);
+    //   if (state.path == "/login") {
+    //     try {
+    //       await authRepository.reSignIn();
+    //       return "/controllers";
+    //     } on LoginSessionExpired catch (_) {
+    //       return "/login";
+    //     }
+    //   }
+    //   return state.path;
+    // },
   );
 }
 
