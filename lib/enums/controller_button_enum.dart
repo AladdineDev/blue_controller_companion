9bd5dd548b4a4fcb32238c13f3ed4185976c453d
AladdineDev <aaldn.dev@gmail.com>
2024-07-21 11:42:59 +0200

    fix: button

--- lib/enums/controller_button_enum.dart
+++ lib/enums/controller_button_enum.dart
@@ -1,22 +1,22 @@
 // ignore_for_file: constant_identifier_names
 
 enum ControllerButton {
-  L1(command: 0x1),
-  R1(command: 0x2),
-  L2(command: 0x3),
-  R2(command: 0x4),
-  L3(command: 0x5),
-  R3(command: 0x6),
-  CROSS(command: 0x7),
-  SQUARE(command: 0x8),
-  CIRCLE(command: 0x9),
-  TRIANGLE(command: 0xa),
-  UP(command: 0xb),
-  DOWN(command: 0xc),
-  LEFT(command: 0xd),
-  RIGHT(command: 0xe),
-  START(command: 0xf),
-  OPTIONS(command: 0x10);
+  CROSS(command: 0x1),
+  CIRCLE(command: 0x2),
+  SQUARE(command: 0x3),
+  TRIANGLE(command: 0x4),
+  L1(command: 0x5),
+  R1(command: 0x6),
+  L2(command: 0x7),
+  R2(command: 0x8),
+  SELECT(command: 0x9),
+  START(command: 0xa),
+  L3(command: 0xb),
+  R3(command: 0xc),
+  HAT_UP(command: 0xd),
+  HAT_DOWN(command: 0xe),
+  HAT_LEFT(command: 0xf),
+  HAT_RIGHT(command: 0x10);
 
   const ControllerButton({required this.command});
 
